fdb0f3f4411ef40d9937d204a2ef946caf5c92c0
Stanislav Okunev <stanislav.okunev@softnet.by>
2022-10-20 04:57:59 +0300

    0.1.0

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const tar = require('tar');
+
+module.exports = class W5MFTypesGeneratePlugin {
+  constructor(options) {
+    this.options = options; // publicDir, typesDir, archiveFile
+  }
+
+  apply(compiler) {
+    const PUBLIC_DIR = this.options?.publicDir || (process.env.NODE_ENV === 'production' ? 'build' : 'public');
+    const TYPES_DIR = this.options?.typesDir || '@types';
+    const ARCHIVE_FILE = this.options?.archiveFile || '@types.tar';
+
+    compiler.hooks.assetEmitted.tap("W5MFTypesGenerate", async (compilation) => {
+      await tar.create({ gzip: false, file: `${PUBLIC_DIR}/${ARCHIVE_FILE}` }, [TYPES_DIR]);
+    });
+  }
+};
