985b69d50bb24ba55462ecfeb6faa33362d306f9
Stanislav Okunev <stanislav.okunev@softnet.by>
2022-12-08 18:51:16 +0300

    0.2.0

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
+const fs = require('fs');
+const path = require('path');
 const tar = require('tar');
 
 module.exports = class W5MFTypesGeneratePlugin {
   constructor(options) {
-    this.options = options; // publicDir, typesDir, archiveFile
+    this.options = options; // publicDir, typesDir, archiveFile, appName
   }
 
   apply(compiler) {
-    const PUBLIC_DIR = this.options?.publicDir || (process.env.NODE_ENV === 'production' ? 'build' : 'public');
-    const TYPES_DIR = this.options?.typesDir || 'w5mf-types';
-    const ARCHIVE_FILE = this.options?.archiveFile || 'w5mf-types.tar';
+    const APP_NAME = this.options?.appName;
+
+    if (!APP_NAME) {
+      throw new Error('[W5MF-TYPES-GENERATE] App name option is required');
+    }
+
+    const ROOT_DIR = process.cwd();
+    const PUBLIC_DIR = this.options?.publicDir || path.resolve(ROOT_DIR, (process.env.NODE_ENV === 'production' ? 'build' : 'public'));
+    const TYPES_DIR = this.options?.typesDir || path.resolve(ROOT_DIR, 'w5mf-types');
+    const ARCHIVE_FILE = this.options?.archiveFile || path.resolve(PUBLIC_DIR, 'w5mf-types.tar');
 
     compiler.hooks.assetEmitted.tap("W5MFTypesGenerate", async (compilation) => {
-      await tar.create({ gzip: false, file: `${PUBLIC_DIR}/${ARCHIVE_FILE}` }, [TYPES_DIR]);
+      fs.stat(TYPES_DIR, async (error) => {
+        if (error) {
+          throw new Error(`[W5MF-TYPES-GENERATE] Not '${TYPES_DIR}' dir`);
+        }
+
+        process.chdir(TYPES_DIR);
+        await tar.create({ gzip: false, file: ARCHIVE_FILE }, [APP_NAME]);
+      });
     });
   }
 };
